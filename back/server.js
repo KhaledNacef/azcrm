cce4eb5710a458339b6c557b15389df8802ec394
khaled nacef <khaledncf0@gmail.com>
2025-05-28 12:32:21 +0100

    done

--- back/server.js
+++ back/server.js
@@ -39,8 +39,8 @@ app.use(xss()); // XSS protection
 app.use(hpp()); // HTTP param pollution
 
 // 5. Body parsing with limits
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 
 // ========================
 // 🏽 Other Middleware
